b166f3219b1da21d971c30ca403002bac2aa21aa
Harsh Lahre <harshlahre109@gmail.com>
2025-01-21 22:42:15 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,9 @@ export default function Header() {
   const handleToggleMenu = () => {
     setOn(!on);
     const body: HTMLElement | any = document.querySelector("body");
-    body.classList.toggle("mobile-nav-active");
+    if (body) {
+      body.classList.toggle("mobile-nav-active");
+    }
   };
 
   return (
